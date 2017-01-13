e0c1ddb35eac1c1ae486d21d9512625d1870c318
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-01-13 08:54:39 -0800

    write first two questions in html and first attempt at their js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var whichCharacter = function (barneyq, tedq) {
-  if (barneyq === "single") {
-    return "Barney";
+  if (rubyq === "front") {
+    return "Ruby";
+  } else if (phpq === "heavy") {
+    return "PHP";
   } else if (tedq === "romance") {
     return "Ted";
   } else {
@@ -12,9 +14,10 @@ $(document).ready(function(){
   $(".quiz").submit(function(event){
     event.preventDefault();
 
-    var marriedQuestion = $("input:radio[name=future]:checked").val()
+    var frontOrBackQuestion = $("input:radio[name=future]:checked").val()
+    var contentQuestion = $("input:radio[name=hobby]:checked").val()
     var hobbyQuestion = $("input:radio[name=hobby]:checked").val()
 
-    $("#result").text("You are " + whichCharacter(marriedQuestion,hobbyQuestion) +"!")
+    $("#result").text("You are " + whichCharacter(frontOrBackQuestion,contentQuestion,hobbyQuestion) +"!")
   });
 });
